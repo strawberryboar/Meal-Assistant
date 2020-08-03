99241f6cdaa098ea23394259ad759ad2afc0a70e
Vincent McGargill <vincentmcgargill@gmail.com>
2020-08-03 16:00:23 -0600

    Completed the meal backend.

---
+++ meal-catalog.js
@@ -0,0 +1,126 @@
+const MealCatalog = document.getElementById("MealCatalog");
+const MealSearchresults = document.getElementById("MealSearchresults");
+const MealDetails = document.getElementById("MealDetails");
+const ReturnMealCatalog = document.getElementById("ReturnMealCatalog");
+
+function GenerateMealCatagories() {
+    const APIurl = "https://www.themealdb.com/api/json/v1/1/categories.php";
+
+    $.ajax({
+        url: APIurl,
+        method: 'GET'
+    }).then(function(response) {
+        $(".MealCategories").append($("<h1>").text("Category List:"));
+        for (i = 0; i < response.categories.length; i++) {
+            var catagory = $("<div>");
+            catagory.val(response.categories[i].strCategory)
+            catagory.attr("class", "MealCategory")
+            var title = $("<h2>").text(response.categories[i].strCategory);
+            catagory.append(title);
+            var description = $("<p>").text(response.categories[i].strCategoryDescription);
+            catagory.append(description);
+            var img = $("<img>").attr("src", response.categories[i].strCategoryThumb)
+            catagory.append(img);
+            $(".MealCategories").append(catagory);
+        }
+
+    });
+}
+
+function GenerateMealAreas() {
+    var APIurl = "https://www.themealdb.com/api/json/v1/1/list.php?a=list";
+
+    $.ajax({
+        url: APIurl,
+        method: 'GET'
+    }).then(function(response) {
+        $(".MealAreas").append($("<h1>").text('Areas List:'));
+        for (i = 0; i < response.meals.length; i++) {
+            var area = $("<div>");
+            area.attr("class", "MealArea");
+            area.val(response.meals[i].strArea);
+            var title = $("<h2>").text(response.meals[i].strArea);
+            area.append(title);
+            $(".MealAreas").append(area);
+        }
+    });
+}
+
+function GenerateMealIngredients() {
+    var APIurl = "https://www.themealdb.com/api/json/v1/1/list.php?i=list";
+
+    $.ajax({
+        url: APIurl,
+        method: 'GET'
+    }).then(function(response) {
+        $(".MealIngredientList").append($("<h1>").text("Ingredients List:"));
+        for (i = 0; i < response.meals.length; i++) {
+            var ul = $("<ul>")
+            var ingredient = $("<li>").text(response.meals[i].strIngredient);
+            ul.append(ingredient);
+            $(".MealIngredientList").append(ul);
+        }
+
+    });
+}
+
+
+$(document).ready(function(){
+    GenerateMealCatagories();
+    GenerateMealAreas();
+    GenerateMealIngredients();
+});
+
+$(document).on('click','.MealCategory',function(){
+    MealCatalog.classList.add("hidden");
+    ReturnMealCatalog.classList.remove("hidden");
+    MealSearchresults.classList.remove("hidden");
+    var category = $(this).val();
+    var APIurl = "https://www.themealdb.com/api/json/v1/1/filter.php?c=" + category
+    GenerateSearchResults(APIurl)
+});
+
+$(document).on('click','.MealArea',function(){
+    MealCatalog.classList.add("hidden");
+    ReturnMealCatalog.classList.remove("hidden");
+    MealSearchresults.classList.remove("hidden");
+    var area = $(this).val();
+    var APIurl = "https://www.themealdb.com/api/json/v1/1/filter.php?a=" + area;
+    GenerateSearchResults(APIurl);
+});
+
+$(document).on('click','#MealRandomGenerator',function(){
+    MealCatalog.classList.add("hidden");
+    ReturnMealCatalog.classList.remove("hidden");
+    MealSearchresults.classList.remove("hidden");
+    var APIurl = "https://www.themealdb.com/api/json/v1/1/random.php";
+    GenerateSearchResults(APIurl);
+});
+
+$(document).on('click','.ReturnMealResults',function(){
+    MealDetails.classList.add("hidden");
+    ReturnMealCatalog.classList.remove("hidden");
+    MealSearchresults.classList.remove("hidden");
+    $("#MealSource").html("");
+    $("#MealVideo").html("");
+    $("#MealIMG").attr("src", "");
+    $("#IngredientList").html("");
+});
+
+// Meal Search Results click Event
+$(document).on('click','.MealSearchResult',function(){
+    var id = $(this).attr('id');
+    var APIurl = "https://www.themealdb.com/api/json/v1/1/lookup.php?i=" + id;
+    ReturnMealCatalog.classList.add("hidden");
+    MealSearchresults.classList.add("hidden");
+    GenerateMealDetails(APIurl);
+    MealDetails.classList.remove("hidden");
+});
+
+// Shows meal Results
+$(document).on('click','#ReturnMealCatalog',function(){
+    MealDetails.classList.add("hidden");
+    MealSearchresults.classList.add("hidden");
+    ReturnMealCatalog.classList.add("hidden");
+    MealCatalog.classList.remove("hidden");
+});
---
+++ meal-scripts.js
@@ -0,0 +1,85 @@
+// Generate Search Results Using a URL
+function GenerateSearchResults(APIurl) {
+    var searchresults = $("#MealSearchresults")
+    searchresults.html("");
+    var ErrorMsg = $("#MealErrorMsg");
+    ErrorMsg.text("");
+    
+    $.ajax({
+        url: APIurl,
+        method: 'GET'
+    }).then(function(response) {
+        if (response.meals === null) {
+            var MealSearchInputVal = $("#MealSearchInput").val();
+            ErrorMsg.css("color", "red");
+            ErrorMsg.text('Error: No search results found for ' + MealSearchInputVal + '. Please try again.')
+        } else {
+            console.log(response)
+            for (i = 0; i < response.meals.length; i++) {
+                var MealObject = response.meals[i];
+                var ResultDiv = $("<div>");
+                ResultDiv.attr("id", MealObject.idMeal);
+                ResultDiv.attr("class", "MealSearchResult");
+                var title = $("<h1>").text(MealObject.strMeal);
+                title.attr("id", MealObject.idMeal);
+                ResultDiv.append(title);
+
+                if (MealObject.strArea !== undefined && MealObject.strCategory !== undefined) {
+                    var description = $("<p>").text("Area: " + MealObject.strArea 
+                    + " - Category: " + MealObject.strCategory);
+                    ResultDiv.append(description);
+                }
+
+                var img = $("<img>").attr("src", MealObject.strMealThumb);
+                img.width(150);
+                ResultDiv.append(img);
+                searchresults.append(ResultDiv);
+            }
+        }
+    });
+}
+
+// Generate Specific Food Details Using A URL
+function GenerateMealDetails(APIurl) {
+    $.ajax({
+        url: APIurl,
+        method: 'GET'
+    }).then(function(response) {
+        var MealObject = response.meals[0];
+        $("#MealTitle").text(MealObject.strMeal);
+        $("#MealIMG").attr("src", MealObject.strMealThumb);
+        $("#MealIMG").width(300);
+        $("#MealArea").text("Area: " + MealObject.strArea);
+        $("#MealCategory").text("Category: " + MealObject.strCategory);
+        var IngredientArray = [MealObject.strIngredient1, MealObject.strIngredient2,
+            MealObject.strIngredient3, MealObject.strIngredient4, MealObject.strIngredient5,
+            MealObject.strIngredient6, MealObject.strIngredient7, MealObject.strIngredient8,
+            MealObject.strIngredient9, MealObject.strIngredient10, MealObject.strIngredient11,
+            MealObject.strIngredient12, MealObject.strIngredient13, MealObject.strIngredient14,
+            MealObject.strIngredient15, MealObject.strIngredient16, MealObject.strIngredient17,
+            MealObject.strIngredient18, MealObject.strIngredient19, MealObject.strIngredient20];
+        var MeasurementArray = [MealObject.strMeasure1, MealObject.strMeasure2, 
+            MealObject.strMeasure3, MealObject.strMeasure4, MealObject.strMeasure5, 
+            MealObject.strMeasure6, MealObject.strMeasure7, MealObject.strMeasure8, 
+            MealObject.strMeasure9, MealObject.strMeasure10, MealObject.strMeasure11,
+            MealObject.strMeasure12, MealObject.strMeasure13, MealObject.strMeasure14, 
+            MealObject.strMeasure15, MealObject.strMeasure16, MealObject.strMeasure17, 
+            MealObject.strMeasure18, MealObject.strMeasure19, MealObject.strMeasure20];
+        for (i = 0; i < IngredientArray.length; i++) {
+            if (IngredientArray[i] !== null && IngredientArray[i] !== "") {
+                var li = $("<li>").text(IngredientArray[i] + " - " + MeasurementArray[i]);
+                $("#IngredientList").append(li);
+            }
+        }
+        $("#MealInstructions").text(MealObject.strInstructions);
+        var source = $("<a>").text("Original Source");
+        source.attr("href", MealObject.strSource);
+        $("#MealSource").append(source);
+        var video = $("<a>").text("YouTube Video");
+        video.attr("href", MealObject.strYoutube);
+        $("#MealVideo").append(video);
+    });
+
+}
+
+
